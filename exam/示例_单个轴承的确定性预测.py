134fde35306a6788b364cf8fe20d2b722d4c711d
HoldenMcGorin <13426831967@163.com>
2024-03-05 16:59:07 +0800

    更新markdown

--- exam/示例_单个轴承的确定性预测.py
+++ exam/示例_单个轴承的确定性预测.py
@@ -3,7 +3,7 @@ from rulframework.data_manager.raw.XJTUDataLoader import XJTUDataLoader
 from rulframework.data_manager.train.SlideWindowDataGenerator import SlideWindowDataGenerator
 from rulframework.entity.Bearing import PredictHistory
 from rulframework.model.PytorchModel import PytorchModel
-from rulframework.model.mlp.MLP_64_48_32 import MLP_64_48_32
+from rulframework.model.mlp.MLP_60_48_32 import MLP_60_48_32
 from rulframework.predictor.RollingPredictor import RollingPredictor
 from rulframework.stage_calculator.BearingStageCalculator import BearingStageCalculator
 from rulframework.stage_calculator.eol.NinetyFivePercentRMSEoLCalculator import NinetyFivePercentRMSEoLCalculator
@@ -24,18 +24,18 @@ if __name__ == '__main__':
     bearing.plot_feature()
 
     # 生成训练数据
-    data_generator = SlideWindowDataGenerator(96)
+    data_generator = SlideWindowDataGenerator(92)
     bearing.train_data = data_generator.generate_data(bearing.feature_data)
 
     # 定义模型并训练
-    model = PytorchModel(MLP_64_48_32())
+    model = PytorchModel(MLP_60_48_32())
     model.train(bearing.train_data.iloc[:, :-32], bearing.train_data.iloc[:, -32:], 10000)
     model.plot_loss()
 
     # 使用预测器进行预测
     predictor = RollingPredictor(model)
-    input_data = bearing.feature_data.iloc[:, 0].tolist()[0:64]
+    input_data = bearing.feature_data.iloc[:, 0].tolist()[0:60]
     prediction = predictor.predict_till_threshold(input_data, bearing.stage_data.failure_threshold_feature)
-    bearing.predict_history = PredictHistory(64, prediction)
+    bearing.predict_history = PredictHistory(59, prediction)
 
     bearing.plot_feature()
